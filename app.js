14f96508fd34dea6efe789399514bbd9e3ea9093
maivlys <122049862+maivlys@users.noreply.github.com>
2024-10-21 11:08:51 +0200

    remove resizing option and enable colorful line

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let userInput = document.querySelector("#user-input");
-const resizeBtn = document.querySelector("#resize-btn");
 const screenContainer = document.querySelector("#screen");
 
 const blackColor = document.querySelector("#black");
@@ -23,13 +21,6 @@ resetBtn.addEventListener("click", () => {
 window.onload = () => {
   createScreen(60);
 };
-
-resizeBtn.addEventListener("click", () => {
-  let width = userInput.value;
-  width = Number(width);
-  userInput.value = "";
-  createScreen(width);
-});
 
 function createScreen(width) {
   while (screenContainer.hasChildNodes()) {
@@ -59,7 +50,7 @@ function draw(color) {
           item.style.backgroundColor = "#ffffffe1";
           break;
         case "rgb":
-          item.style.backgroundColor = "#2cdcf3da";
+          item.style.backgroundColor = randomColor();
           break;
         case "eraser":
           item.style.backgroundColor = "#ffffff8c";
@@ -73,4 +64,19 @@ function draw(color) {
   });
 }
 
-function changeColor(params) {}
+function randomColor() {
+  let randomNo = Math.floor(Math.random() * 4 + 1);
+  switch (randomNo) {
+    case 1:
+      return "#7A93F0";
+    case 2:
+      return "#7AF08C";
+    case 3:
+      return "#F07A96";
+    case 4:
+      return "#F0D17A";
+
+    default:
+      "#595E70";
+  }
+}
